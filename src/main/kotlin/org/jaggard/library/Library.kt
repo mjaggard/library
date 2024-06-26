596e91ec82d5b213b50638c6ea69f320fa68aa5a
Mat Jaggard <mat@billforward.net>
2024-06-26 10:55:59 +0100

    Add book to library

--- src/main/kotlin/org/jaggard/library/Library.kt
+++ src/main/kotlin/org/jaggard/library/Library.kt
@@ -1,5 +1,7 @@
 package org.jaggard.library
 
+import java.util.concurrent.ConcurrentHashMap
+
 /**
  * This class is responsible for the datastore. Normally this would be in a database of some kind.
  * Each map represents a database index.
@@ -8,6 +10,14 @@ package org.jaggard.library
  * This, along with the functions in this class ensure that we never end up with an inconsistent state and,
  * if anything did go wrong, there's only one instance of each book stored in this class.
  */
-class Library(private val byIsbn: Map<String, Book> = mutableMapOf()) {
+class Library(private val byIsbn: MutableMap<String, Book> = ConcurrentHashMap()) {
 
+    fun addBook(book: Book) {
+        val previous = byIsbn.putIfAbsent(book.isbn, book)
+        if (previous != null) {
+            throw IllegalStateException("Book with ISBN '${book.isbn}' is already in this library.")
+        }
+    }
+
+    fun size() = byIsbn.size
 }
--- src/test/kotlin/org/jaggard/library/LibraryTest.kt
+++ src/test/kotlin/org/jaggard/library/LibraryTest.kt
@@ -1,14 +1,33 @@
 package org.jaggard.library
 
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchIllegalStateException
 import org.junit.jupiter.api.Test
 
 class LibraryTest {
 
     @Test
-    fun addBook() {
+    fun addBooksSuccess() {
         val lib = Library()
         val book = Book("ISBN 0-09-186777-0", "Tony Hawks", "Round Ireland with a fridge")
+        val book2 = Book("ISBN 0-7475-8108-8", "J.K. Rowling", "Harry Potter and the Half-Blood Prince")
+        lib.addBook(book)
+        lib.addBook(book2)
 
-        //lib.addBook(book)
+        assertThat(lib.size())
+            .isEqualTo(2)
+    }
+
+    @Test
+    fun addBookFailure() {
+        val lib = Library()
+        val isbn = "ISBN 0-09-186777-0"
+        val book = Book(isbn, "Tony Hawks", "Round Ireland with a fridge")
+        val book2 = Book(isbn /*wrong*/, "J.K. Rowling", "Harry Potter and the Half-Blood Prince")
+        lib.addBook(book)
+        val exception = catchIllegalStateException { lib.addBook(book2) }
+        assertThat(exception)
+            .isNotNull()
+            .hasMessage("Book with ISBN 'ISBN 0-09-186777-0' is already in this library.")
     }
 }
