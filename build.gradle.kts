2bb594e65825775d431c43815a32a846dd5cf88d
Mat Jaggard <mat@billforward.net>
2024-06-26 09:13:35 +0100

    Empty Kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.jaggard"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
